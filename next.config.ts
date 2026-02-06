94ed168201ff2fd07da1003f154ab9c59954742d
Fabiano Guedes <prfabianoguedes@gmail.com>
2026-02-05 21:42:41 -0300

    fix: turbopack config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   // Configurações para evitar processamento indevido de pacotes externos no servidor
   serverExternalPackages: ["@uploadthing/react", "uploadthing"],
 
+  // Isso aqui avisa ao Next.js que está tudo bem usar Webpack mesmo com o Turbopack ativo
+  turbopack: {},
+
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md$/,
