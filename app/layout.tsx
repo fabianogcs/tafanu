99796dd3c953ca96be276363fe7c663e4a551e11
Fabiano Guedes <prfabianoguedes@gmail.com>
2026-05-20 10:25:40 -0300

    Ajustes pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import AffiliateTracker from "@/components/AffiliateTracker";
 import { Suspense } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import MetaPixel from "@/components/MetaPixel";
 
 export const metadata: Metadata = {
   metadataBase: new URL(
@@ -125,6 +126,7 @@ export default async function RootLayout({
           <Footer />
           <Analytics />
           <SpeedInsights />
+          <MetaPixel />
           <CookieBanner />
         </Providers>
 
